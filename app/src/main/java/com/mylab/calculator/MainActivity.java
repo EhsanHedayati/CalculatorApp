80ce8d5bc1be57cfc7e801579c992ebf2a00ad4e
Ehsan <ehsan.hedayati83@gmail.com>
2020-10-02 17:44:07 +0330

    import classes & initializing text view.

--- app/src/main/java/com/mylab/calculator/MainActivity.java
+++ app/src/main/java/com/mylab/calculator/MainActivity.java
@@ -3,17 +3,24 @@ package com.mylab.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     
    private Button btn;
    private TextView tv1;
-   private EditText et1; 
+   private EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = findViewById(R.id.tv1);
+
+
         
         
         
